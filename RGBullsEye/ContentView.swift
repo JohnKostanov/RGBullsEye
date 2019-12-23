94522de68d2aa7c10cb307a7d0fa678a6e9ef394
JohnKostanov <kostanovd@icloud.com>
2019-12-23 18:50:59 +0100

    Add making reusable views

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     @State var rGuess: Double
     @State var gGuess: Double
     @State var bGuess: Double
-    
+        
     var body: some View {
         VStack {
             HStack {
@@ -29,25 +29,37 @@ struct ContentView: View {
                 VStack {
                     Color(red: rGuess, green: gGuess, blue: bGuess)
                     VStack {
-                        Text("R: 127 G: 127 B: 127")
+                        Text("R: \(Int(rGuess * 255)) G: \(Int(gGuess * 255)) B: \(Int(bGuess * 255))")
                     }
                 }
             }
             Button(action: {}) {
                 Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
             }
-            HStack {
-                Text("0").foregroundColor(.red)
-                Slider(value: $rGuess)
-                Text("255").foregroundColor(.red)
-            }.padding(.horizontal)
+            ColorSlider(value: $rGuess, textColor: .red)
+            ColorSlider(value: $gGuess, textColor: .green)
+            ColorSlider(value: $bGuess, textColor: .blue)
+
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(rGuess: 0.8, gGuess: 0.5, bGuess: 0.5).previewLayout(.fixed(width: 568, height: 320))
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5).previewLayout(.fixed(width: 568, height: 320))
     }
 }
 
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255").foregroundColor(textColor)
+        }.padding(.horizontal)
+    }
+}
